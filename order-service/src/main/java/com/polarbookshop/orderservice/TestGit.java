4b0d67c12ae01e300add41e46934440605567def
ajavaguy <ajavaguy96@gmail.com>
2023-11-13 22:05:06 +0700

    T2

--- order-service/src/main/java/com/polarbookshop/orderservice/TestGit.java
+++ order-service/src/main/java/com/polarbookshop/orderservice/TestGit.java
@@ -1,2 +1,11 @@
-package com.polarbookshop.orderservice;public class TestGit {
+package com.polarbookshop.orderservice;
+
+public class TestGit {
+    public boolean test() {
+        return true;
+    }
+
+    public boolean test2() {
+        return true;
+    }
 }
